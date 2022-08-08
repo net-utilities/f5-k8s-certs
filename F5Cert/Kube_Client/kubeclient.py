184fd6456366e5d8caba7d0752078417f977a859
Patrik Jonsson <patrik.jonsson@gmail.com>
2022-08-08 22:44:26 +0200

    Changing to list destructuring instead of dictionaries Removing redundant f5-cert label

--- F5Cert/Kube_Client/kubeclient.py
+++ F5Cert/Kube_Client/kubeclient.py
@@ -32,19 +32,19 @@ class KubeClient:
         data = self.get_secret(name)
         username = data.get('F5_PASSWORD')
         password = data.get('F5_PASSWORD')
-        return {
-            'username': base64.b64decode(username).decode('utf-8'),
-            'password': base64.b64decode(password).decode('utf-8')
-        }
+        return [
+            base64.b64decode(username).decode('utf-8'),
+            base64.b64decode(password).decode('utf-8'),
+        ]
 
     def get_cert_key(self, name):
         data = self.get_secret(name)
         certificate = data.get('tls.crt')
         key = data.get('tls.key')
-        return {
-            'cert': base64.b64decode(certificate),
-            'key': base64.b64decode(key)
-        }
+        return [
+            base64.b64decode(certificate),
+            base64.b64decode(key)
+        ]
 
     def get_certificates(self):
         logger.debug(f'Getting {self.cluster_url}/apis/cert-manager.io/v1/namespaces/{self.namespace}/certificates')
@@ -60,7 +60,7 @@ class KubeClient:
             if not 'labels' in c['metadata']:
                 continue
             labels = c['metadata']['labels']
-            if labels.get('f5-cert') != 'true':
+            if labels.get('f5-cert-type') != 'management':
                 continue
 
             f5_cert_type = labels.get('f5-cert-type')
@@ -71,11 +71,11 @@ class KubeClient:
             credentials = self.get_f5_credentials(f5_auth_ref)
             cert_key = self.get_cert_key(cert_secret_name)
 
-            certificates.append({
-                'cert_type': f5_cert_type,
-                'device_fqdn': f5_device_fqdn,
-                'credentials': credentials,
-                'cert_key': cert_key,
-            })
+            certificates.append([
+                f5_cert_type,
+                f5_device_fqdn,
+                credentials,
+                cert_key,
+            ])
 
         return certificates
--- main.py
+++ main.py
@@ -4,26 +4,29 @@ from F5Cert import F5rest, logger, KubeClient, Settings
 [token, cluster_api_url, namespace] = Settings().get_settings()
 
 kube_client = KubeClient(token, cluster_api_url, namespace)
-
 certificates = kube_client.get_certificates()
 
 for c in certificates:
 
-    if c['cert_type'] != 'management':
+    [f5_cert_type, f5_device_fqdn, credentials, cert_key] = c
+    [username, password] = credentials
+    [cert, key] = cert_key
+
+    if f5_cert_type != 'management':
         logger.error('Certificate other than management certificates are not supported yet')
         continue
 
     f5rest = F5rest(
-        c['credentials']['username'],
-        c['credentials']['password'],
-        c['device_fqdn'],
+        username,
+        password,
+        f5_device_fqdn,
         'management',
         False
     )
 
     try:
-        f5rest.update_management_cert(c['cert_key']['cert'],
-            c['cert_key']['key'])
+        f5rest.update_management_cert(cert,
+                                      key)
     except BaseException as e:
         logger.error(f'Failed to update management certificate on {c["device_fqdn"]}')
         logger.error(e)
